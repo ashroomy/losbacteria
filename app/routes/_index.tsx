aefdd58cfd020ba3888e3d58ede01269b1486e7c
Andrea <andrea.zuniga@higlobe.com>
2024-07-25 15:24:11 -0600

    Update styles

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -9,12 +9,12 @@ export const meta: MetaFunction = () => {
 export default function Index() {
   return (
     <main>
-    <div className="font-kiffoR flex align-middle justify-center  bg-#1E1E1E">
-      <div className="flex flex-col gap-3 xs:p-5 md:p-6 md:max-w-[600px] ">
-      <p className="text-primary text-25">Si estás leyendo esto, felicidades, oficialmente has sido infectado. Ya no hay vuelta atrás. Lo único que te queda, es dejar tu correo para enterarte de lo que viene.  </p>
+    <div className="font-kiffoR flex align-left justify-start  bg-#1E1E1E">
+      <div className="flex flex-col gap-3 xs:p-5 md:p-6 md:max-w-[600px] text-2xl">
+      <p className="text-primary ">Si estás leyendo esto, felicidades, oficialmente has sido infectado. Ya no hay vuelta atrás. Lo único que te queda, es dejar tu correo para enterarte de lo que viene.  </p>
         <form className="flex flex-col gap-2">
-          <input placeholder="Tu correo aqui" className="w-[220px] bg-dark border-primary text-primary border "/>
-          <button className="text-primary w-fit	 text-25" type="submit">Notificame</button>
+          <input placeholder="Tu correo aqui" className="w-[220px] bg-dark border-primary text-primary border-b color-[rgba(200, 255, 0, 0.25)] p-[5px] "/>
+          <button className="text-primary w-fit	 text-2xl	" type="submit">Notificame</button>
         </form> 
       </div>
 

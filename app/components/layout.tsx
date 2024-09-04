caeefda14bac2ec96939c92d65c23fd1d299aef4
Andrea <andrea.zuniga@higlobe.com>
2024-09-03 20:10:42 -0600

    bg opacity

--- app/components/layout.tsx
+++ app/components/layout.tsx
@@ -1,9 +1,10 @@
 import { Navigation } from "./navigation"
 
 export function Layout({ children }: { children: React.ReactNode }) {
-	return <div className="">
+	return <div className="relative">
 		<Navigation/>
-		<div className="bg-squares bg-opacity-5">
+		<div  className="bg-squares bg-opacity-5 h-full absolute w-full opacity-[0.5]"></div>
+		<div>
 			{children}
 
 		</div>
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -116,19 +116,21 @@ export default function Home() {
 
   }
 
-  const rotateVariant = {
-    animate: {
-      x: ['', '-90vw'],
-      transition: {
-        x: {
-          repeat: Infinity,
-          repeatType: "loop",
-          duration: 10,
-          ease: "linear",
-        },
-      },
-    }
-  };
+  // const rotateVariant = {
+  //   animate: {
+  //     rotate: [0, 90, 180,90, 0 ],
+  //     transition: {
+  //       x: {
+  //         repeat: Infinity,
+  //         repeatType: "loop",
+  //         duration: 10,
+  //         times: [0, 0.2, 0.5, 0.8, 1],
+
+  //         ease: "easeInOut",
+  //       },
+  //     },
+  //   }
+  // };
 
 
   const AnimatedImg = ({ active, setActive, id, x, y, width, height , src, angle}:any) => {
@@ -141,29 +143,27 @@ export default function Home() {
   
   // const rotate = useTransform(x, [angle, ], [0, 2])
     const size= width;
-    const dx = useSpring(x, springConfig);
-    const dy = useSpring(y, springConfig);
+    const dx = useSpring(x+10, springConfig);
+    const dy = useSpring(y+10, springConfig);
+    console.log('ANDGLE', angle)
     return (
       <motion.img
-        src={src}
+         src={src}
         drag
         dragConstraints={constraintsRef}
         dragTransition={{ bounceStiffness: 100, bounceDamping: 5 }}
         dragElastic={0.8}
-        whileTap={{ cursor: "grabbing", scale:1.5 }}
+        whileTap={{ cursor: "grabbing", scale:1.3 }}
         onDragStart={() => setActive(id)}
         style={{
           width: width,
           height: height,
           x: isDragging ? x : dx,
           y: isDragging ? y : dy,
-          top:  y+gap,
-          left: x+gap,
           zIndex: isDragging ? 1 : 0,
-          rotate:`${angle}deg`,
-          cursor: "grab",
-          flexShrink:0
-        }}
+          rotate:`${angle}deg`
+        }
+      }
       />
       )
 
@@ -172,30 +172,29 @@ export default function Home() {
   return (
     <Layout>
 
-      <div className="flex-col h-full  ">
+      <div className="flex-col h-full ">
       <motion.div
         ref={constraintsRef}
         style={{
-          width: "98%",
+          width: "99vw",
           padding: "0",
           margin: "0",
           height: "85vh",
           position: "relative",
-          display:"flex",
-          gap:'5px',
+          display:"flex", 
+          justifyContent:"center",
           flexFlow:"wrap",
           pageBreakInside:'avoid',
           marginTop:"auto"
         }}
-        // variants={container}
-        initial="hidden"
-        animate="show"
       >
 
-        <AnimatedImg  src={candy} angle="-82.974" height={sizes.candy.height} size={sizes.candy.size} width={sizes.candy.width} id="candy-1" x={candyX} y={candyY} active={active}  setActive={setActive} />
+        <AnimatedImg  src={candy} angle="-82.974" height={sizes.candy.height} width={sizes.candy.width} id="candy-1" x={candyX} y={candyY} active={active}  setActive={setActive} />
         <AnimatedImg src={gum} angle="-12.013" width={sizes.gum.width} height={sizes.gum.height}  id="gum-1" x={gumX} y={gumY} active={active}  setActive={setActive} />
         <AnimatedImg src={soda} angle="-22.608" width={sizes.soda.width}  height={sizes.soda.height}  id="soda-1" x={sodaX} y={sodaY} active={active}  setActive={setActive} />
         <AnimatedImg src={banana} angle="-88.182" width={sizes.banana.width} height={sizes.banana.height} id="banana-1" x={bananaX} y={bananaY} active={active}  setActive={setActive} />
+        <AnimatedImg  src={fries} angle="53.385" width={sizes.fries.width}   height={sizes.fries.height}   id="fries-1" x={friesX} y={friesY} active={active}  setActive={setActive} />
+
         <AnimatedImg src={banana} angle="110.164" width={sizes.banana.width}  height={sizes.banana.height}  id="banana-2" x={bananaX1} y={bananaY1} active={active}  setActive={setActive} />
         <AnimatedImg  src={bloodbag} angle="-2.89" width={sizes.bloodbag.width}   height={sizes.bloodbag.height}   id="bloodbag-1" x={bloodbagX} y={bloodbagY} active={active}  setActive={setActive} />
         <AnimatedImg  src={candy} angle="-71.974" width={sizes.candy.width}   height={sizes.candy.height}   id="candy-2" x={candyX1} y={candyY1} active={active}  setActive={setActive} />
@@ -209,101 +208,7 @@ export default function Home() {
         <AnimatedImg src={gum} angle="-69.551" width={sizes.gum.width}    height={sizes.gum.height}  id="gum-2" x={gumX1} y={gumY1} active={active}  setActive={setActive} />
 
 
-      {/* <motion.img   
-        src={cereal}
-        className=""
-        style={{
-          minWidth:sizes.cereal.width,
-          minHeight: sizes.cereal.height,
-          width:sizes.cereal.width,
-          height:'fit-content',
-          transform: 'rotate(-3.435deg)',
       
-        }} 
-        drag
-        dragConstraints={constraintsRef}
-        /> */}
-      {/* <motion.img   
-        src={spray}
-        style={{
-          minWidth:sizes.spray.width,
-          minHeight: sizes.spray.height,
-          maxWidth:sizes.spray.width,
-          height:'fit-content',
-          transform: 'rotate(54.328deg)'
-        }} 
-        /> */}
-{/* 
-      <motion.img   
-        src={fries}
-        style={{
-          minWidth:sizes.fries.width,
-          minHeight: sizes.fries.height,
-          maxWidth:sizes.fries.width,
-          height:'fit-content',
-          transform: 'rotate(53.385deg)'
-        }} /> */}
-         
-        {/* <motion.img   
-        src={gum}
-        style={{
-          minWidth:sizes.gum.width,
-          minHeight: sizes.gum.height,
-          maxWidth:sizes.gum.width,
-          height:'fit-content',
-          transform: 'rotate(-69.551deg)'
-        }} /> */}
-
-
-
-        {/* <motion.img
-          src={cereal}
-          style={{ width: 107, height: 151,  position: 'absolute'}}
-          // animate={{
-          //   rotate: [0, 180, 50 , -28],
-          //   x: [700, 250, 60, 7],
-          //   y: [0, 200 ,maxHeight-200],
-          //   position:'absolute'
-
-          // }}
-          drag
-          dragTransition={{ bounceStiffness: 200, bounceDamping: 20 }}
-          dragElastic={1}
-          dragConstraints={constraintsRef}
-          variants={cerealVar}
-        />
-        <motion.img src={bloodbag} style={{ width: 113, height: 122 }}
-          drag
-          dragConstraints={constraintsRef}
-          variants={rowOne}
-        />
-        <motion.img src={soda}
-          drag
-          dragConstraints={constraintsRef}
-          dragElastic={0.2}
-          style={{ width: 112, height: 159, left: 200, rotate: 47.97, position: 'absolute' }} variants={rowOne} transition={{ duration: 2, type: 'spring', bounce: 0.8 }} />
-
-        <motion.img src={candy}
-          drag
-          dragConstraints={constraintsRef}
-          style={{ width: 112, height: 159, left: 45, rotate: -43.98, position: 'absolute' }} variants={rowTwo} transition={{ duration: 2, type: 'spring', bounce: 0.5 }} />
-        <motion.img
-          drag
-          dragConstraints={constraintsRef}
-          src={fries} style={{ width: 112, height: 159, left: 190, rotate: 21.5, position: 'absolute' }} variants={rowTwo} transition={{ duration: 2, type: 'spring', bounce: 0.9 }} />
-
-        <motion.img
-          drag
-          dragConstraints={constraintsRef}
-          src={banana} style={{ width: 112, height: 159, left: 10, rotate: -2.02, position: 'absolute' }} variants={rowThree} transition={{ duration: 2, type: 'spring', bounce: 0.5 }} />
-        <motion.img drag
-          dragConstraints={constraintsRef}
-          src={spray} style={{ width: 122, height: 182, left: 80, rotate: -32.62, position: 'absolute' }} variants={rowThree} transition={{ duration: 2, type: 'spring', bounce: 0.6 }} />
-        <motion.img drag
-          dragConstraints={constraintsRef}
-          src={gum} style={{ width: 138, height: 154, left: 186, rotate: -37, position: 'absolute' }} variants={rowThree} transition={{ duration: 2, type: 'spring', bounce: 0.7 }} />  */}
-
-
       </motion.div>
         
 
